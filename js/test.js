7c3f7f9f593daded5fcc3c0f68fc3e81ae875b72
cemGr <66869029+cemGr@users.noreply.github.com>
2020-08-31 19:55:54 +0200

    update

--- js/test.js
+++ js/test.js
@@ -1,6 +1,6 @@
 //var a = document.getElementById("result").innerHTML;
 var a=0;
-var bildschirm="0";
+var bildschirm="";
 
 function hallo(c){
     bildschirm+=c;
@@ -10,15 +10,34 @@ function hallo(c){
 function mathexpression(e){
   switch (e) {
     case 10:
+      bildschirm+='+';
+      document.getElementById("result").innerHTML= bildschirm;
+    break;
+
     case 11:
+      bildschirm+='-';
+      document.getElementById("result").innerHTML= bildschirm;
+    break;
     case 12:
+      bildschirm+='*';
+      document.getElementById("result").innerHTML= bildschirm;
+    break;
     case 13:
+      bildschirm+='/';
+      document.getElementById("result").innerHTML= bildschirm;
+    break;
     case 14:
+      bildschirm+='=';
+    break;
     case 15:
+      bildschirm+='%';
+      document.getElementById("result").innerHTML= bildschirm;
+    break;
     case 16:
-
-      break;
-    default:
-
+      bildschirm='';
+      document.getElementById("result").innerHTML= bildschirm;
+    break;
+    default: alert("error");
+    break;
   }
 };
