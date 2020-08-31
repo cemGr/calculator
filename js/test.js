52e88b5e6a4441fe73b232d0f0ba4dd6d7d72106
cemGr <66869029+cemGr@users.noreply.github.com>
2020-08-31 19:32:39 +0200

    calculator

---
+++ js/test.js
@@ -0,0 +1,24 @@
+//var a = document.getElementById("result").innerHTML;
+var a=0;
+var bildschirm="0";
+
+function hallo(c){
+    bildschirm+=c;
+    document.getElementById("result").innerHTML= bildschirm;
+};
+
+function mathexpression(e){
+  switch (e) {
+    case 10:
+    case 11:
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+
+      break;
+    default:
+
+  }
+};
