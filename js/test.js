9d186b455c1e458f92d0a00da3ea06886d5ab477
cemGr <66869029+cemGr@users.noreply.github.com>
2020-09-01 13:28:47 +0200

    (fast) funktionstüchtiger taschenrechner
    rechnungen funktionieren, jedoch muss eine regel noch definiert werden, wie sich expressions nebeneinander verhalten

--- js/test.js
+++ js/test.js
@@ -1,8 +1,11 @@
 //var a = document.getElementById("result").innerHTML;
-var rechnung=0;
+var rechnung="";
 var bildschirm="";
+var safer;
 
+//Bildschirmausgabe
 function hallo(c){
+    rechnung+=c;
     bildschirm+=c;
     document.getElementById("result").innerHTML= bildschirm;
 };
@@ -11,37 +14,105 @@ function mathexpression(e){
   switch (e) {
     case 10:
       bildschirm+='+';
-      rechnung+='#+';
+      rechnung+='#+#';
       document.getElementById("result").innerHTML= bildschirm;
     break;
 
     case 11:
       bildschirm+='-';
-        rechnung+='#-';
+        rechnung+='#-#';
       document.getElementById("result").innerHTML= bildschirm;
     break;
     case 12:
       bildschirm+='*';
-      rechnung+='#*';
+      rechnung+='#*#';
       document.getElementById("result").innerHTML= bildschirm;
     break;
     case 13:
       bildschirm+='/';
-        rechnung+='#/';
+        rechnung+='#/#';
       document.getElementById("result").innerHTML= bildschirm;
     break;
     case 14:
+      calculate();
     break;
     case 15:
       bildschirm+='%';
-      rechnung+='#%';
+      rechnung+='#%#';
       document.getElementById("result").innerHTML= bildschirm;
     break;
     case 16:
-      bildschirm='';
+      bildschirm='0';
+      rechnung='';
+      safer=undefined;
       document.getElementById("result").innerHTML= bildschirm;
+      bildschirm='';
     break;
     default: alert("error");
     break;
   }
+};
+
+function calculate(){
+
+  safer=rechnung.split("#");
+  //now we are calculating
+
+//strenge rechenregeln
+  for(i=0;safer.length>i;i++){
+    if(safer[i]=='*'){
+      safer[i+1]=parseFloat(safer[i-1])* parseFloat(safer[i+1]);
+      safer[i-1]="$";
+      safer[i]="$";
+      alert("mul");
+    }
+
+    else if(safer[i]=='/'){
+      safer[i+1]=parseFloat(safer[i-1])/parseFloat(safer[i+1]);
+      safer[i-1]="$";
+      safer[i]="$";
+      alert("div");
+
+    }
+    else if(safer[i]=='%'){
+      safer[i+1]=parseInt(safer[i-1]) % parseInt(safer[i+1]);
+      safer[i-1]="$";
+      safer[i]="$";
+      alert("modulo");
+    }
+  }
+      /// $$$$1+1+$$4+5
+      //$$$$##2+$$4+5
+      //$$$$####$$6+5
+      //$$$$####$$##11
+      //lastindex end;
+      //rechnung für die laschen rechenregeln
+
+  for(i=0;safer.length>i;i++){
+  //safer wieder cutten für die einfache rechnung
+    if(safer[i]=='+'){
+      for(g=i+1;g<safer.length;g++){
+        if(safer[g]!="$"){
+          console.log(safer[g]);
+          safer[g]= parseInt(safer[g])+ parseInt(safer[i-1]);
+          safer[i]='#';
+          safer[i-1]='#';
+          alert("addition");
+          break;
+        }
+      }
+    }
+    else if(safer[i]=='-'){
+      for(g=i+1;g<safer.length;g++){
+        if(safer[g]!="$"){
+          safer[g]=parseFloat(safer[g])-parseFloat(safer[i-1]);
+          safer[i]='#';
+          safer[i-1]='#';
+          alert("subtraktion");
+          break;
+        }
+      }
+    }
+  }
+        document.getElementById("result").innerHTML=safer[safer.length-1];
 };
